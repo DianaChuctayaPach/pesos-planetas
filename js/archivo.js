e200a04a9a5aaff3343578709debcb796897ae2e
MelanieOC <melanie.ocharan@gmail.com>
2017-07-18 10:59:11 -0500

    funcionando

--- js/archivo.js
+++ js/archivo.js
@@ -1,47 +1,39 @@
-var peso = Number(prompt("CUAL ES TU PESO?"));
-var planeta = Number(prompt("Elige tu planeta:\n 1 es marte\n 2 es jupiter \n 3 es mercurio \n 4 es venus \n 5 es saturno \n 6 urano \n 7 neptuno \n 8 pluton"));
-var g_tierra = 9.8;
-var gravedad;
-var peso_final;
-var nombre;
+function calcular() {
+  var g_tierra = 9.8;
+  var gravedad;
+  var peso_final;
+  var nombre;
+  var peso = document.getElementById("peso").value;
+  var planeta = document.getElementById("planetas").value;
+  if(planeta==1){
+    gravedad= 3.7;
+    nombre = "Marte";
+  } else if (planeta==2){
+    gravedad = 24.8;
+    nombre = "Jupiter";
+  }else if (planeta==3){
+    gravedad = 3.72;
+    nombre = "Mercurio";
+  }else if (planeta==4){
+    gravedad = 8.82;
+    nombre = "Venus";
+  }else if (planeta==5){
+    gravedad = 9.01;
+    nombre = "Saturno";
+  }else if (planeta==6){
+    gravedad = 8.72;
+    nombre = "Urano";
+  }else if (planeta==7){
+    gravedad = 10.97;
+    nombre = "Neptuno";
+  }else if (planeta==8){
+    gravedad = 0.65;
+    nombre = "Pluton";
+  } else {
+    alert ("Esa opción no existe");
+  }
 
-switch(planeta){
-case 1:
-  gravedad= 3.7;
-   nombre = "Marte";
-break;
-case 2:
-  gravedad = 24.8;
-  nombre = "Jupiter";
-break;
-case 3:
-  gravedad = 3.72;
-  nombre = "Mercurio";
-break;
-case 4:
-  gravedad = 8.82;
-  nombre = "Venus";
-break;
-case 5:
-  gravedad = 9.01;
-  nombre = "Saturno";
-break;
-case 6:
-  gravedad = 8.72;
-  nombre = "Urano";
-break;
-case 7:
-  gravedad = 10.97;
-  nombre = "Neptuno";
-break;
-case 8:
-  gravedad = 0.65;
-  nombre = "Pluton";
-break;
-default:
-  alert ("Esa opción no existe");
+  peso_final = peso * gravedad / g_tierra;
+  var respuesta = '<br>'+"Tu peso en "  + nombre + " es <strong> " + peso_final.toFixed(2) + "</strong> Kilos";
+  document.getElementById('resultado').innerHTML=respuesta;
 }
-
-peso_final = peso * gravedad / g_tierra;
-
-document.write("Tu peso en "  + nombre + " es <strong> " + peso_final.toFixed(2) + "</strong> Kilos");
